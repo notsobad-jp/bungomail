49676d38076e379c09ca6efd619cb0195d7f240e
notsobad <tomomichi.onishi@gmail.com>
2021-07-31 13:28:36 +0900

    fix unsubscribe error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def destroy
-    @user = User.find_by(email: params[:email])
     begin
+      @user = User.find_by(email: params[:email])
+      if @user.blank?
+        flash[:error] = '入力されたメールアドレスで登録が確認できませんでした。入力内容をご確認いただき、それでも解決しない場合はお手数ですが運営までお問い合わせください。'
+        redirect_to page_path(:unsubscribe) and return
+      end
+
       # 有料ユーザーのときは処理をスキップして手動削除
       if @user.stripe_customer_id
         logger.error "[Error] Paid account cancelled: #{@user.stripe_customer_id}"
@@ -11,7 +16,7 @@ class UsersController < ApplicationController
       flash[:success] = '退会処理を完了しました。すべての課金とメール配信を停止します。これまでのご利用ありがとうございました。'
       redirect_to params[:redirect_to] || root_path
     rescue => e
-      logger.error "[Error]Unsubscription failed: #{e.message}, #{current_user.id}"
+      logger.error "[Error]Unsubscription failed: #{e.message}, #{params[:email]}"
       flash[:error] = '処理に失敗しました。。何回か試してもうまくいかない場合、お手数ですが運営までお問い合わせください。'
       redirect_to page_path(:unsubscribe)
     end
