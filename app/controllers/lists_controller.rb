e8c810ab5d1e578a627a8d11b6e7cfa566bc758e
notsobad <tomomichi.onishi@gmail.com>
2021-07-31 23:55:54 +0900

    update lists#show replacing campaign_groups to book_assignments

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,7 @@ class ListsController < ApplicationController
   def books
     year = params[:year] || Time.current.year
     start = Time.current.change(year: year).beginning_of_year
-    @campaign_groups = CampaignGroup.includes(:book).where(list_id: params[:id], start_at: start..start.end_of_year).where("start_at < ?", Time.current).order(:start_at)
+    @book_assignments = BookAssignment.includes(:book).where(channel_id: Channel.find_by(code: 'bungomail-official'), start_date: start..start.end_of_year).where("start_date < ?", Time.zone.today).order(:start_date)
     @meta_title = "過去配信作品（#{year}）"
   end
 end
