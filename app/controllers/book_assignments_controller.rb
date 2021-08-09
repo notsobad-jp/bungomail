53bf5e8bd18322045f37be3bd4cf5c09531654cd
notsobad <tomomichi.onishi@gmail.com>
2021-08-09 16:12:23 +0900

    validate delivery start_date and trial_start_date

--- app/controllers/book_assignments_controller.rb
+++ app/controllers/book_assignments_controller.rb
@@ -1,7 +1,9 @@
 class BookAssignmentsController < ApplicationController
   def create
-    # TODO: トライアル予約済み(開始前)の人も配信予約できるようにする
-    @user = User.where.(paid_member: true).find_by(email: ba_params[:email])
+    # 有料会員（トライアル含む）or トライアル開始前（予約済み）の人が配信予約可能
+    @user = User.where(email: ba_params[:email]).merge(
+      User.where(paid_member: true).or(User.where("trial_start_date >= ?", Date.current))
+    ).first
     raise ActiveRecord::RecordNotFound.new("有料プランの登録が確認できませんでした。カスタム配信を利用する際は、事前に#{view_context.link_to 'ブンゴウメール有料プランへの登録', memberships_new_path, class: 'text-link'}が必要です。") if !@user
 
     @channel = Channel.where(id: @user.id, user_id: @user.id).first_or_create
