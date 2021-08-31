b4f2f358a1b8e3ce53be48ecc967afa0715aaa4b
o_tomomichi <tomomichi.onishi@gmail.com>
2021-09-01 00:08:38 +0900

    Feature/sync subscription status (#160)
    * refactor active_emails_in_stripe, add by_unpaid_users scope to channels & subs
    
    * add paid_without_official_subscription scope to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :channels, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
+  scope :activated_in_stripe, -> (active_emails) { where(paid_member: false).where(email: active_emails) }  # stripeで購読したけどまだDBの支払いステータスに反映されていないuser
+  scope :canceled_in_stripe, -> (active_emails) { where(paid_member: true).where.not(email: active_emails) }  # stripeで解約したけどまだDBの支払いステータスに反映されていないuser
+  scope :paid_without_official_subscription, -> { where(paid_member: true).left_outer_joins(:subscriptions).where(subscriptions: {id: nil}) } # 有料プランなのに公式チャネル購読してないuser
 
   validates :email, presence: true, uniqueness: true
 
@@ -30,5 +33,19 @@ class User < ApplicationRecord
 
   def digest
     Digest::SHA256.hexdigest(email)
+  end
+
+
+  class << self
+    # stripeで支払い中のメールアドレス一覧
+    def active_emails_in_stripe
+      emails = []
+      subscriptions = Stripe::Subscription.list({limit: 100, expand: ['data.customer']})
+      subscriptions.auto_paging_each do |sub|
+        emails << sub.customer.email
+      end
+      raise 'Email duplicated!' if emails.length != emails.uniq.length
+      emails
+    end
   end
 end
