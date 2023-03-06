76db3c5b6267b514cd9a3a82c5eefee5d70e4b08
notsobad <tomomichi.onishi@gmail.com>
2023-03-06 22:29:50 +0900

    add routes for letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
     get 'books', on: :member
   end
 
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   get ':page' => "pages#show", as: :page
   root to: 'pages#lp'
 end
