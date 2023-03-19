095f5705da605882bdf2a40c6e1f0ab7591889e8
notsobad <tomomichi.onishi@gmail.com>
2023-03-20 03:47:41 +0900

    fix controllers

--- app/controllers/book_assignments_controller.rb
+++ app/controllers/book_assignments_controller.rb
@@ -2,13 +2,11 @@ class BookAssignmentsController < ApplicationController
   def create
     # 有料会員（トライアル含む）or トライアル開始前（予約済み）の人が配信予約可能
     @user = User.where(email: ba_params[:email]).merge(
-      User.where(paid_member: true).or(User.where("trial_start_date >= ?", Date.current))
+      User.basic.or(User.where("trial_start_date >= ?", Date.current))
     ).first
     raise ActiveRecord::RecordNotFound.new("有料プランの登録が確認できませんでした。カスタム配信を利用する際は、事前に#{view_context.link_to 'ブンゴウメール有料プランへの登録', new_user_path, class: 'text-link'}が必要です。") if !@user
 
-    @channel = Channel.where(id: @user.id, user_id: @user.id).first_or_create
-    @user.subscriptions.where(channel_id: @channel.id).first_or_create
-    @ba = @channel.book_assignments.create!(
+    @ba = @user.book_assignments.create!(
       book_id: ba_params[:book_id],
       book_type: ba_params[:book_type],
       start_date: ba_params[:start_date],
@@ -27,7 +25,7 @@ class BookAssignmentsController < ApplicationController
 
   def cancel
     @ba = BookAssignment.find_by(id: params[:id])
-    raise ActiveRecord::RecordNotFound.new('【エラー】配信が見つかりませんでした。。解決しない場合は運営までお問い合わせください。') if !@ba || @ba.channel.code == 'bungomail-official'
+    raise ActiveRecord::RecordNotFound.new('【エラー】配信が見つかりませんでした。。解決しない場合は運営までお問い合わせください。') if !@ba || @ba.user.email == "info@notsobad.jp"
     @meta_title = '配信停止'
     @breadcrumbs = [ {text: 'カスタム配信', link: page_path(:custom_delivery)}, {text: @meta_title} ]
   rescue => e
@@ -38,9 +36,9 @@ class BookAssignmentsController < ApplicationController
 
   def destroy
     @ba = BookAssignment.find_by(id: params[:id])
-    raise ActiveRecord::RecordNotFound.new('【エラー】配信が見つかりませんでした。。解決しない場合は運営までお問い合わせください。') if !@ba || @ba.channel.code == 'bungomail-official'
+    raise ActiveRecord::RecordNotFound.new('【エラー】配信が見つかりませんでした。。解決しない場合は運営までお問い合わせください。') if !@ba || @ba.user.email == "info@notsobad.jp"
     @ba.destroy!
-    BungoMailer.with(user: @ba.channel.user, author_title: "#{@ba.book.author}『#{@ba.book.title}』", delivery_period: "#{@ba.start_date} 〜 #{@ba.end_date}").schedule_canceled_email.deliver_later
+    BungoMailer.with(user: @ba.user, author_title: "#{@ba.book.author}『#{@ba.book.title}』", delivery_period: "#{@ba.start_date} 〜 #{@ba.end_date}").schedule_canceled_email.deliver_later
     flash[:success] = '配信をキャンセルしました！'
     redirect_to page_path(:book_assignment_canceled)
   rescue => e
@@ -52,6 +50,6 @@ class BookAssignmentsController < ApplicationController
   private
 
   def ba_params
-    params.require(:book_assignment).permit(:book_id, :book_type, :channel_id, :start_date, :end_date, :email, :delivery_time)
+    params.require(:book_assignment).permit(:book_id, :book_type, :user_id, :start_date, :end_date, :email, :delivery_time)
   end
 end
