7bc752d611ddfe85920c5a87cbc3b217d6bc22d5
notsobad <tomomichi.onishi@gmail.com>
2023-03-25 22:42:08 +0900

    change header menu

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   resources :magic_tokens
   resource :user
 
-  get 'signup' => 'users#new'
-  get 'campaigns/dogramagra' => "pages#dogramagra"
+  get 'auth' => 'magic_tokens#auth'
   get 'login' => 'magic_tokens#new'
   delete 'logout' => 'magic_tokens#destroy'
-  get 'auth' => 'magic_tokens#auth'
+  get 'mypage' => 'users#show'
+  get 'signup' => 'users#new'
+
+  get 'campaigns/dogramagra' => "pages#dogramagra"
 
   # TODO: 新システム移行後は不要
   resources :lists do
