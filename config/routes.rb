bb57ae48af950a831896a28dbb9ff320152a3fc7
notsobad <tomomichi.onishi@gmail.com>
2023-04-09 22:08:13 +0900

    add feeds page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :books, only: [:show]
-  resources :book_assignments do
+  resources :book_assignments, shallow: true do
     get :cancel, on: :member
+    resources :feeds
   end
   resources :magic_tokens
   resource :user
