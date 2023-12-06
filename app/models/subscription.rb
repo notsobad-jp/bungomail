14c97cf6a8d4d8f461e938038d6d14b678c605d5
o_tomomichi <tomomichi.onishi@gmail.com>
2023-12-06 15:13:44 +0900

    refactor: unify DelayedJob ID (#230)
    * remove webpush_job_id and rename job_id
    
    * refactor jobs
    
    * fix specs
    
    * use FeedDelivery job

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,4 @@ class Subscription < ApplicationRecord
   belongs_to :book_assignment
 
   enum delivery_method: { "Eメール" => "email", "プッシュ通知" => "webpush" }
-
-  scope :deliver_by_email, -> { where(delivery_method: :email) }
-  scope :deliver_by_webpush, -> { where(delivery_method: :webpush) }
 end
