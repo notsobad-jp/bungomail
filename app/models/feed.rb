53188a07fe4fd8b197a02b33e46a36b7fc82b832
notsobad <tomomichi.onishi@gmail.com>
2023-12-06 14:56:28 +0900

    refactor jobs

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -11,6 +11,11 @@ class Feed < ApplicationRecord
     self.delayed_job&.delete
   end
 
+  def deliver
+    BungoMailer.with(feed: self).feed_email.deliver_now
+    deliver_webpush
+  end
+
   def index
     (delivery_date - book_assignment.start_date).to_i + 1
   end
@@ -18,13 +23,8 @@ class Feed < ApplicationRecord
   def schedule
     return if self.send_at < Time.current
 
-    res_email = BungoMailer.with(feed: self).feed_email.deliver_later(queue: 'feed_email', wait_until: self.send_at)
-    res_webpush = WebPushJob.set(wait_until: self.send_at).perform_later(feed: self, payload: webpush_payload)
-
-    self.update!(
-      delayed_job_email_id: res_email.provider_job_id,
-      delayed_job_webpush_id: res_webpush.provider_job_id,
-    )
+    job_id = delay(run_at: self.send_at).deliver
+    self.update!(delayed_job_id: job_id)
   end
 
   def send_at
@@ -33,6 +33,43 @@ class Feed < ApplicationRecord
 
   private
 
+    def deliver_webpush
+      users = book_assignment.subscriptions.where(delivery_method: :webpush).preload(:user).map(&:user)
+      return if users.blank?
+
+      payload_json = JSON.generate(webpush_payload)
+      public_key = Rails.application.credentials.dig(:vapid, :public_key)
+      private_key = Rails.application.credentials.dig(:vapid, :private_key)
+
+      users.each do |user|
+        next if user.webpush_endpoint.blank? || user.webpush_p256dh.blank? || user.webpush_auth.blank?
+
+        begin
+          WebPush.payload_send(
+            message: payload_json,
+            endpoint: user.webpush_endpoint,
+            p256dh: user.webpush_p256dh,
+            auth: user.webpush_auth,
+            vapid: {
+              subject: "mailto:info@notsobad.jp",
+              public_key: public_key,
+              private_key: private_key,
+              expiration: 12 * 60 * 60,
+            },
+          )
+        rescue => exception
+          # WebPushに失敗したら、ユーザーのWebPush情報を削除して通知メールを送信する
+          Rails.logger.warn("WebPushJob failed: [#{user.id}] #{exception.message}")
+          user.update!(webpush_endpoint: nil, webpush_p256dh: nil, webpush_auth: nil)
+          BungoMailer.with(user: user).webpush_failed_email.deliver_later
+        end
+      end
+    end
+
+    def host
+      Rails.env.production? ? "https://bungomail.com" : "http://localhost:3000"
+    end
+
     def webpush_payload
       {
         title: "#{book_assignment.book.author_name}『#{book_assignment.book.title}』",
@@ -40,9 +77,5 @@ class Feed < ApplicationRecord
         icon: "https://bungomail.com/favicon.ico",
         url: feed_url(id, host: host),
       }
-    end
-
-    def host
-      Rails.env.production? ? "https://bungomail.com" : "http://localhost:3000"
     end
 end
