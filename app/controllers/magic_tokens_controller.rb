cda46df5f17e226a94aedb58bf1d32d470ad3860
o_tomomichi <tomomichi.onishi@gmail.com>
2023-12-12 17:28:30 +0900

    feat: onetime token login (#238)
    * add token login
    
    * clear magic token after use
    
    * refactor

--- app/controllers/magic_tokens_controller.rb
+++ app/controllers/magic_tokens_controller.rb
@@ -7,21 +7,22 @@ class MagicTokensController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    return redirect_to login_path, flash: { error: 'メールアドレスが見つかりませんでした。。初めての方はアカウント登録してください。' } unless @user
+    user = User.find_by(email: params[:email])
+    redirect_to login_path, flash: { error: 'メールアドレスが見つかりませんでした。。初めての方はアカウント登録してください。' } and return unless user
 
-    @user.generate_magic_login_token!
-    BungoMailer.with(user: @user).magic_login_email.deliver_later
+    user.generate_magic_login_token!
+    BungoMailer.with(user: user).magic_login_email.deliver_later
     flash[:success] = 'ログイン用URLを送信しました！メールに記載されたURLからログインしてください。'
     redirect_to login_path
   end
 
   def auth
     @token = params[:token]
-    @user = User.load_from_magic_login_token(params[:token])
-    return not_authenticated if @user.blank?
+    user = User.load_from_magic_login_token(params[:token])
+    redirect_to login_path, flash: { error: 'ログインに失敗しました。。もう一度メール送信をお試しください。' } and return unless user
 
-    auto_login(@user)
+    auto_login(user)
+    user.clear_magic_login_token!
     flash[:success] = 'ログインしました！'
     redirect_to mypage_path
   end
