28f3792c65bbdcfab79fa15c4a74c56291260214
notsobad <tomomichi.onishi@gmail.com>
2024-01-28 18:40:28 +0900

    refactor: remove twitter_share_url

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -74,19 +74,6 @@ class Distribution < ApplicationRecord
     end
   end
 
-  def twitter_short_url
-    begin
-      Bitly.call(path: 'shorten', params: { long_url: self.twitter_long_url })
-    rescue => e
-      logger.error "[Error] Bitly API failed: #{e}"
-    end
-  end
-
-  def twitter_long_url
-    "https://twitter.com/intent/tweet?url=https%3A%2F%2Fbungomail.com%2F&hashtags=ブンゴウメール,青空文庫&text=#{start_date.month}月は%20%23#{book.author_name}%20%23#{book.title}%20を配信中！"
-  end
-
-
   private
 
   # 同一チャネルで期間が重複するレコードが存在すればinvalid(Freeプランのみ)
