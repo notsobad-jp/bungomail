9e1e20b9d6abd4451dc1b0098f2f117cdea0d00b
o_tomomichi <tomomichi.onishi@gmail.com>
2025-01-02 10:32:23 +0900

    refactor: remove old tables (#272)

--- app/models/subject.rb
+++
@@ -1,3 +0,0 @@
-class Subject < ApplicationRecord
-  has_and_belongs_to_many :guten_books
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_02_004153) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_02_012822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "aozora_books", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "author", null: false
-    t.bigint "author_id"
-    t.bigint "file_id"
-    t.text "footnote"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.integer "words_count", default: 0
-    t.string "beginning"
-    t.integer "access_count", default: 0
-    t.string "category_id"
-    t.boolean "rights_reserved", default: false
-    t.string "first_edition"
-    t.integer "published_at"
-    t.string "character_type"
-    t.boolean "juvenile", default: false, null: false
-    t.string "sub_title"
-    t.integer "canonical_book_id"
-    t.string "source"
-    t.index ["access_count"], name: "index_aozora_books_on_access_count"
-    t.index ["canonical_book_id"], name: "index_aozora_books_on_canonical_book_id"
-    t.index ["category_id"], name: "index_aozora_books_on_category_id"
-    t.index ["character_type"], name: "index_aozora_books_on_character_type"
-    t.index ["juvenile"], name: "index_aozora_books_on_juvenile"
-    t.index ["published_at"], name: "index_aozora_books_on_published_at"
-    t.index ["words_count"], name: "index_aozora_books_on_words_count"
-  end
 
   create_table "campaigns", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.integer "book_id", null: false
@@ -78,14 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_02_004153) do
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "email_digests", primary_key: "digest", id: :string, force: :cascade do |t|
-    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.boolean "trial_ended", default: false, null: false
-    t.datetime "canceled_at", precision: nil
-    t.index ["canceled_at"], name: "index_email_digests_on_canceled_at"
-  end
-
   create_table "feeds", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "campaign_id", null: false
     t.uuid "delayed_job_id"
@@ -97,46 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_02_004153) do
     t.text "comment"
     t.index ["campaign_id"], name: "index_feeds_on_campaign_id"
     t.index ["delayed_job_id"], name: "index_feeds_on_delayed_job_id"
-  end
-
-  create_table "guten_books", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "author"
-    t.boolean "rights_reserved", default: false
-    t.string "language", default: "en"
-    t.bigint "downloads", default: 0
-    t.integer "words_count", default: 0, null: false
-    t.integer "chars_count", default: 0, null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.integer "author_id"
-    t.string "category_id"
-    t.string "beginning"
-    t.integer "unique_words_count", default: 0
-    t.integer "ngsl_words_count", default: 0
-    t.float "ngsl_ratio"
-    t.integer "birth_year"
-    t.integer "death_year"
-    t.integer "canonical_book_id"
-    t.string "sub_title"
-    t.index ["author_id"], name: "index_guten_books_on_author_id"
-    t.index ["canonical_book_id"], name: "index_guten_books_on_canonical_book_id"
-    t.index ["category_id"], name: "index_guten_books_on_category_id"
-    t.index ["ngsl_ratio"], name: "index_guten_books_on_ngsl_ratio"
-  end
-
-  create_table "guten_books_subjects", id: false, force: :cascade do |t|
-    t.bigint "guten_book_id"
-    t.string "subject_id"
-    t.index ["guten_book_id", "subject_id"], name: "index_guten_books_subjects_on_guten_book_id_and_subject_id", unique: true
-    t.index ["guten_book_id"], name: "index_guten_books_subjects_on_guten_book_id"
-    t.index ["subject_id"], name: "index_guten_books_subjects_on_subject_id"
-  end
-
-  create_table "subjects", id: :string, force: :cascade do |t|
-    t.integer "books_count", default: 0
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "subscriptions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -169,13 +92,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_02_004153) do
     t.index ["magic_login_token"], name: "index_users_on_magic_login_token"
   end
 
-  add_foreign_key "aozora_books", "aozora_books", column: "canonical_book_id", deferrable: :deferred
   add_foreign_key "campaigns", "users"
   add_foreign_key "feeds", "campaigns", on_delete: :cascade
   add_foreign_key "feeds", "delayed_jobs", on_delete: :nullify
-  add_foreign_key "guten_books", "guten_books", column: "canonical_book_id"
-  add_foreign_key "guten_books_subjects", "guten_books", on_delete: :cascade
-  add_foreign_key "guten_books_subjects", "subjects", on_delete: :cascade
   add_foreign_key "subscriptions", "campaigns"
   add_foreign_key "subscriptions", "users"
 end
