eedcd6d7e53f2f8200d9f5af6c87ad304441d3a5
notsobad <tomomichi.onishi@gmail.com>
2025-01-03 21:11:18 +0900

    fix dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,10 +34,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.delivery_method = :letter_opener
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :letter_opener
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
